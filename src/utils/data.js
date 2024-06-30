f94c931f37c60c1a7477d3d58e1bfb6216f5e557
Deelip Kumar Singh <deelipsingh77@gmail.com>
2024-06-30 17:32:13 +0530

    Minor Data Change

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,16 +1,3 @@
-const seriesA = {
-  data: [2, 3, 1, 4, 5],
-  label: "Series A",
-};
-const seriesB = {
-  data: [3, 1, 4, 2, 1],
-  label: "Series B",
-};
-const seriesC = {
-  data: [3, 2, 4, 5, 1],
-  label: "Series C",
-};
-
 const gauge = {
   rc: 51,
   tp: 41,
@@ -62,4 +49,4 @@ const monthlyProductionData = [
 ];
 
 
-export { seriesA, seriesB, seriesC, gauge, data, monthlyProductionData };
+export { gauge, data, monthlyProductionData };
